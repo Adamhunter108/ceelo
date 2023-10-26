85648153479fc813bc7ef6b3472e59b8a1ec8677
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2023-10-26 19:23:06 -0400

    die side images and main texture component

---
+++ app/components/Dice.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+
+interface DiceProps {
+  number: number;
+}
+
+const Dice: React.FC<DiceProps> = ({ number }) => {
+  const diceTextures = [
+    useLoader(TextureLoader, "/img/sides/1.png"),
+    useLoader(TextureLoader, "/img/sides/2.png"),
+    useLoader(TextureLoader, "/img/sides/3.png"),
+    useLoader(TextureLoader, "/img/sides/4.png"),
+    useLoader(TextureLoader, "/img/sides/5.png"),
+    useLoader(TextureLoader, "/img/sides/6.png"),
+  ];
+
+  // Select the appropriate texture based on the number
+  let currentTexture = diceTextures[number - 1];
+
+  return (
+    <mesh>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial map={currentTexture} />
+    </mesh>
+  );
+};
+
+export default Dice;
