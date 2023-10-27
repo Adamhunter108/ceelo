9e8662842b2a9664561b217d048a3376c3dce207
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2023-10-26 21:36:51 -0400

    trying to render 3D boxes
    not working yet

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import GameScene from "./components/GameScene";
+import Box from "./components/Box";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="">
-        <p>Cee Lo</p>
-        <GameScene />
-      </div>
-    </main>
+    // <main className="">
+    //   <div className="">
+    //     <p>Cee Lo</p>
+    //     {/* <GameScene /> */}
+    //     <Box />
+    //   </div>
+    // </main>
+    <div>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
   );
 }
