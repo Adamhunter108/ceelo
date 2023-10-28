4c4d2d8e9fb73e374520aaee0a3b6bbc6a5ec443
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2023-10-28 09:13:42 -0400

    fixed Six

--- app/components/die-sides/Six.tsx
+++ app/components/die-sides/Six.tsx
@@ -8,21 +8,12 @@ export default function Six() {
       <div
         className={`outline outline-2 h-36 w-36 bg-gray-200  rounded-2xl shadow-x2l transform flex justify-between p-4 hover:scale-150 transition duration-700 hover:rotate-180 hover:shadow-2xl ${randomRotateClass()}`}
       >
-        <div className="space-y-4 pt-1">
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-        </div>
-        <div className="space-y-4 pt-1">
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-        </div>
-        <div className="space-y-4 pt-1">
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-          <div className="w-6 h-6 bg-black rounded-full"></div>
-        </div>
+        <div className="absolute w-8 h-8 bg-black rounded-full top-4 left-4"></div>
+        <div className="absolute w-8 h-8 bg-black rounded-full top-4 right-4"></div>
+        <div className="absolute w-8 h-8 bg-black rounded-full bottom-4 left-4"></div>
+        <div className="absolute w-8 h-8 bg-black rounded-full bottom-4 right-4"></div>
+        <div className="absolute w-8 h-8 bg-black rounded-full top-1/2 left-4 transform -translate-y-1/2"></div>
+        <div className="absolute w-8 h-8 bg-black rounded-full top-1/2 right-4 transform -translate-y-1/2"></div>
       </div>
     </div>
   );
