cc27a6017a65f8bbd17bdb7e56bd9cbc6335e9d5
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2024-04-10 12:05:57 -0400

    Rounds won dialog modal

--- app/components/ui/two-player/TwoPlayer.tsx
+++ app/components/ui/two-player/TwoPlayer.tsx
@@ -39,9 +39,7 @@ export default function TwoPlayer() {
 
   const [player1WinCount, setPlayer1WinCount] = useState<number>(0);
   const [player2WinCount, setPlayer2WinCount] = useState<number>(0);
-  console.log(`Player 1 wins: ${player1WinCount}`);
-  console.log(`Player 2 wins: ${player2WinCount}`);
-  console.log(`This round's winner: ${winner}`);
+  const [openRoundsWon, setOpenRoundsWon] = useState<boolean>(false);
 
   useEffect(() => {
     if (winner === player1) setPlayer1WinCount((prevCount) => prevCount + 1);
@@ -108,7 +106,8 @@ export default function TwoPlayer() {
 
   const playAgain = () => {
     if (gameOver) {
-      setPlayer1Score(null),
+      setOpenRoundsWon(true),
+        setPlayer1Score(null),
         setPlayer2Score(null),
         setScore(null),
         setGameOver(false),
@@ -236,15 +235,6 @@ export default function TwoPlayer() {
           <div className="flex flex-col items-center">
             <p className="font-sans text-gray-500 text-lg">Player 1</p>
             <p className="-mt-2 text-white text-3xl -rotate-6">{player1}</p>
-            {/* <p className="mt-2 font-sans text-gray-500 text-lg">
-              {player1WinCount !== 0 ? "Rounds won" : ""}
-            </p>
-            <p className="text-white">
-              {player1WinCount !== 0 ? `${player1WinCount}` : ""}
-            </p>
-            <p className="text-gray-500 font-sans">
-              {player1Score !== null ? "score" : ""}
-            </p> */}
             <p
               className={`h-6 pt-1 font-sans ${
                 winner === player1
@@ -260,15 +250,6 @@ export default function TwoPlayer() {
           <div className="flex flex-col items-center">
             <p className="font-sans text-gray-500 text-lg">Player 2</p>
             <p className="-mt-2 text-white text-3xl -rotate-6">{player2}</p>
-            {/* <p className="mt-2 font-sans text-gray-500 text-lg">
-              {player2WinCount !== 0 ? "Rounds won" : ""}
-            </p>
-            <p className="text-white">
-              {player2WinCount !== 0 ? `${player2WinCount}` : ""}
-            </p>
-            <p className="text-gray-500 font-sans">
-              {player2Score !== null ? "score" : ""}
-            </p> */}
             <p
               className={`h-6 pt-1 font-sans ${
                 winner === player2
@@ -377,6 +358,70 @@ export default function TwoPlayer() {
           </button>
         </div>
       )}
+      <Transition.Root show={openRoundsWon} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                  <div>
+                    <div className="mx-auto flex h-24 w-24 items-center justify-center rounded-full bg-green-100 animate-gradientFlow">
+                      <img src="/img/logos/three-dice.png" alt="three dice" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900 font-sans"
+                      >
+                        Rounds Won:
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-lg text-gray-700">
+                          {player1}:{" "}
+                          <span className="font-sans">{player1WinCount}</span>
+                        </p>
+                        <p className="text-lg text-gray-700">
+                          {player2}:{" "}
+                          <span className="font-sans">{player2WinCount}</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-full bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-sans animate-gradientFlow"
+                      onClick={() => setOpenRoundsWon(false)}
+                    >
+                      OK
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   );
 }
